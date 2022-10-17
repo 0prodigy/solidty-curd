c91463f4925a449dcfd3720b7c5500bfe2237814
Akash Pathak <pathakvikash9211@gmail.com>
2022-10-17 23:46:13 +0530

    Test empoloyee implementation

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -8,7 +8,30 @@ async function main() {
 
   await employee.deployed();
 
-  console.log(`deployed to ${employee.address}`);
+  console.log("Employee deployed to:", employee.address);
+
+  const newEmployee = await employee.create(
+    "0x5B38Da6a701c568545dCfcB03FcB875f56beddC4",
+    "Akash",
+    "pathakvikash9211@gmail.com",
+    23,
+  )
+  console.log("Added new Employee: -> ", newEmployee);
+
+  const updateEmployee = await employee.updateEmployee(
+    "0x5B38Da6a701c568545dCfcB03FcB875f56beddC4",
+    "Akash Pathak",
+    "test@gmai.com",
+    23,
+  )
+  console.log("Updated Employee: -> ", updateEmployee);
+
+  const getEmployee = await employee.getEmployee(0);
+  console.log("Get Employee: -> ", getEmployee);
+
+  const deleteEmployee = await employee.deleteEmployee(0);
+  console.log("Deleted Employee: -> ", deleteEmployee);
+
 }
 
 // We recommend this pattern to be able to use async/await everywhere
